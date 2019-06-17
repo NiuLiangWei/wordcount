c83868d6cf1a03ad2b62acf485ead3989c17f907
nlw <1353905492@2qq.com>
2019-06-17 16:03:01 +0800

    6-17

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 def home(request):
-    return render(request,'home.html');
+    return render(request,'home.html')
+def count(requset):
+    text = requset.GET['text']
+    result={}
+    for i in text:
+        if i not in result:
+            result[i]=1
+        else:
+            result[i]+=1
+    result = sorted(result.items(),key=lambda x:x[1], reverse=True)
+    return render(requset, 'count.html',{'count_result':result})
